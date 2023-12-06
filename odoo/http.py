600f9e57ba5120b273ef87aa80dbde0e8f5fcc36
Simon Arnu <{ID}+{username}@users.noreply.github.com>
2023-12-06 14:54:12 +0100

    JRAD-853: reduce session cookie max age from 90 days to 1

--- odoo/http.py
+++ odoo/http.py
@@ -1440,7 +1440,7 @@ class Root(object):
         # - It could allow session fixation attacks.
         if not explicit_session and hasattr(response, 'set_cookie'):
             response.set_cookie(
-                'session_id', httprequest.session.sid, max_age=90 * 24 * 60 * 60, httponly=True)
+                'session_id', httprequest.session.sid, max_age=1 * 24 * 60 * 60, httponly=True)
 
         return response
 
