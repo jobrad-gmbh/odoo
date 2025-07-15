190e6064fc603063e52627df2feb34fb105a0400
Borna Bešić <borna.besic@jobrad.org>
2025-07-15 14:07:33 +0200

    CPT-608 Replace deprecated OrderedMultiDict with MultiDict

--- addons/website/models/website.py
+++ addons/website/models/website.py
@@ -12,7 +12,7 @@ import requests
 from lxml import etree, html
 from psycopg2 import sql
 from werkzeug import urls
-from werkzeug.datastructures import OrderedMultiDict
+from werkzeug.datastructures import MultiDict
 from werkzeug.exceptions import NotFound
 
 from odoo import api, fields, models, tools, http, release, registry
@@ -1391,10 +1391,10 @@ class Website(models.Model):
     def _is_canonical_url(self, canonical_params):
         """Returns whether the current request URL is canonical."""
         self.ensure_one()
-        # Compare OrderedMultiDict because the order is important, there must be
+        # Compare MultiDict (ordered) because the order is important, there must be
         # only one canonical and not params permutations.
         params = request.httprequest.args
-        canonical_params = canonical_params or OrderedMultiDict()
+        canonical_params = canonical_params or MultiDict()
         if params != canonical_params:
             return False
         # Compare URL at the first rerouting iteration (if available) because
